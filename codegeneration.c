68002cc49a9133f1e235e57d2dff47cc355f61a3
Michael-Kolesnikov <kolesnikov_michael@mail.ru>
2025-04-03 00:52:03 +0300

    rewrite codegeneration for some nodes

--- codegeneration.c
+++ codegeneration.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "ast.h"
 
+
+typedef enum {
+    CONTEXT_DEFAULT,
+    CONTEXT_PARAMETERS,
+    CONTEXT_COMPOUND,
+    CONTEXT_FOR,
+} Context;
+
 static int label_counter = 1;
+static FILE* output_file;
+static Context current_context = CONTEXT_DEFAULT;
 
 char* generate_label() {
     char* label = (char*)malloc(10 * sizeof(char));
@@ -9,108 +20,198 @@ char* generate_label() {
     return label;
 }
 
-void generate_code_from_ast(Node* node, FILE* output) {
-    if (!node) return;
+void generate_code(Node* node){
 
     switch (node->type) {
-        case IDENTIFIER_NODE: {
-            IdentifierNode* id_node = (IdentifierNode*)node;
-            fprintf(output, "%s", id_node->name);
-            break;
-        }
-        case CONSTANT_NODE: {
-            ConstantNode* const_node = (ConstantNode*)node;
-            if (const_node->const_type == CONST_INT) {
-                fprintf(output, "%d", const_node->value.i_value);
-            } else if (const_node->const_type == CONST_FLOAT) {
-                fprintf(output, "%f", const_node->value.f_value);
-            }
-            break;
-        }
-        case BINARY_OPERATION_NODE: {
-            BinaryOperationNode* bin_node = (BinaryOperationNode*)node;
-            generate_code_from_ast(bin_node->left, output);
-            fprintf(output, " %c ", bin_node->op);
-            generate_code_from_ast(bin_node->right, output);
-            break;
-        }
-        case DECLARATION_NODE: {
-            DeclarationNode* decl_node = (DeclarationNode*)node;
-            fprintf(output, "%s ",decl_node->type_specifier);
-            generate_code_from_ast(decl_node->identifier, output);
-            if(decl_node->initializer != NULL){
-                fprintf(output, " = ");
-                generate_code_from_ast(decl_node->initializer, output);
-            }
-            fprintf(output, ";\n");
-            break;
-        }
-        case ASSIGNMENT_NODE: {
-            AssignmentNode* assign_node = (AssignmentNode*)node;
-            generate_code_from_ast(assign_node->left, output);
-            fprintf(output, " %s ", assign_node->op);
-            generate_code_from_ast(assign_node->right, output);
-            break;
-        }
-        case LOGICAL_OPERATION_NODE: {
-            LogicalOperationNode* log_node = (LogicalOperationNode*)node;
-            generate_code_from_ast(log_node->left, output);
-            fprintf(output, " %s ", log_node->op);
-            generate_code_from_ast(log_node->right, output);
-            break;
-        }
         case IF_NODE: {
             IfNode* if_node = (IfNode*)node;
-
             char* else_label = generate_label();
             char* end_label = generate_label();
-
-            fprintf(output, "if (");
-            generate_code_from_ast(if_node->condition, output);
-            fprintf(output, ") goto %s;\n", else_label);
-
+            fprintf(output_file, "if (");
+            generate_code(if_node->condition);
+            fprintf(output_file, ") goto %s;\n", else_label);
             if (if_node->else_statement) {
-                generate_code_from_ast(if_node->else_statement, output);
+                generate_code(if_node->else_statement);
             }
-            fprintf(output, "goto %s;\n", end_label);
-            fprintf(output, "%s:\n", else_label);
-
-            generate_code_from_ast(if_node->then_statement, output);
-
-            fprintf(output, "%s:\n", end_label);
-
+            fprintf(output_file, "goto %s;\n", end_label);
+            fprintf(output_file, "%s:\n", else_label);
+            generate_code(if_node->then_statement);
+            fprintf(output_file, "%s:\n", end_label);
             free(else_label);
             free(end_label);
             break;
         }
+        case WHILE_NODE: {
+            WhileNode* while_node = (WhileNode*)node;
+            char* loop_label = generate_label();
+            char* end_label = generate_label();
+            fprintf(output_file, "%s:\n", loop_label);
+            fprintf(output_file, "if(!(");
+            generate_code(while_node->condition);
+            fprintf(output_file, " )) goto %s", end_label);
+            fprintf(output_file, ";\n");
+            generate_code(while_node->body);
+            fprintf(output_file, " goto %s;\n", loop_label);
+            fprintf(output_file, "%s:\n", end_label);
+            break;
+        }
+        case FOR_NODE: {
+            ForNode* for_node = (ForNode*)node;
+            char* loop_label = generate_label();
+            char* end_label = generate_label();
+            current_context = CONTEXT_FOR;
+
+            if (for_node->initialization->type != EMPTY_STATEMENT_NODE) {
+                current_context = CONTEXT_DEFAULT;
+                generate_code(for_node->initialization);
+                current_context = CONTEXT_FOR;
+                fprintf(output_file, "\n");
+            }
+            
+            fprintf(output_file, "%s:\n", loop_label);
+            
+            if (((ExpressionStatementNode*)for_node->condition)->expr->type != EMPTY_STATEMENT_NODE) {
+                fprintf(output_file, "if (!(");
+                generate_code(for_node->condition);
+                fprintf(output_file, ")) goto %s;\n", end_label);
+            }
+            current_context = CONTEXT_DEFAULT;
+            generate_code(for_node->body);
+            current_context = CONTEXT_FOR;
+            if (((ExpressionStatementNode*)for_node->update)->expr->type != EMPTY_STATEMENT_NODE) {
+                generate_code(for_node->update);
+                fprintf(output_file, ";\n");
+            }
+            fprintf(output_file, "goto %s;\n", loop_label);
+            fprintf(output_file, "%s:", end_label);
+            free(loop_label);
+            free(end_label);
+            current_context = CONTEXT_DEFAULT;
+            break;
+        }
+        case ASSIGNMENT_NODE: {
+            AssignmentNode* assignment_node = (AssignmentNode*)node;
+            generate_code(assignment_node->left);
+            fprintf(output_file," %s ",assignment_node->op);
+            generate_code(assignment_node->right);
+            break;
+        }
+        case POSTFIX_INCEMENT_NODE: {
+            PostfixIncrement* incr = (PostfixIncrement*)node;
+            generate_code(incr->expression);
+            fprintf(output_file, "++");
+            break;
+        }
         case EXPRESSION_STATEMENT_NODE: {
-            ExpressionStatementNode* expr_stmt_node = (ExpressionStatementNode*)node;
-            generate_code_from_ast(expr_stmt_node->expr, output);
-            fprintf(output, ";\n");
-            break;
-        }
-        case EMPTY_STATEMENT_NODE: {
-            fprintf(output, ";\n");
-            break;
-        }
-        case COMPOUND_STATEMENT_NODE: {
-            CompoundStatementNode* compound_node = (CompoundStatementNode*)node;
-            for (int i = 0; i < compound_node->count; i++) {
-                generate_code_from_ast(compound_node->statements[i], output);
+            ExpressionStatementNode* expr_statement = (ExpressionStatementNode*)node;
+            generate_code(expr_statement->expr);
+            if(current_context == CONTEXT_DEFAULT && expr_statement->expr->type != EMPTY_STATEMENT_NODE){
+                fprintf(output_file,";\n");
             }
             break;
         }
-        case FUNCTION_DECLARATION_NODE: {
-            FunctionDeclarationNode* func_decl_node = (FunctionDeclarationNode*)node;
-            fprintf(output, "%s ", func_decl_node->return_type);
-            // generate_code_from_ast(func_decl_node->name, output);
-            fprintf(output, "() {\n");
-            generate_code_from_ast(func_decl_node->body, output);
-            fprintf(output, "}\n");
+        case LOGICAL_OPERATION_NODE: {
+            LogicalOperationNode* logical_op = (LogicalOperationNode*)node;
+            generate_code(logical_op->left);
+            fprintf(output_file, " %s ", logical_op->op);
+            generate_code(logical_op->right);
             break;
         }
-        default:
-            fprintf(output, "Unknown node type\n");
+        case FUNCTION_DECLARATION_NODE: {
+            FunctionDeclarationNode* function_declaration = (FunctionDeclarationNode*)node;
+            generate_code(function_declaration->return_type);
+            fprintf(output_file, " ");
+            generate_code(function_declaration->declarator);
+            fprintf(output_file,"{\n");
+            generate_code(function_declaration->body);
+            fprintf(output_file,"}\n");
             break;
+        }
+        case VALUE_NODE: {
+            ValueNode* value = (ValueNode*)node;
+            fprintf(output_file,"%s",value->value);
+            break;
+        }
+        case FUNCTION_DECLARATOR_NODE: {
+            FunctionDeclaratorNode* function_declarator = (FunctionDeclaratorNode*)node;
+            generate_code(function_declarator->declarator);
+            fprintf(output_file, "(");
+            current_context = CONTEXT_PARAMETERS;
+            generate_code(function_declarator->parameters);
+            current_context = CONTEXT_DEFAULT;
+            fprintf(output_file, ")");
+            break;
+        }
+        case PARAMETERS_NODE: {
+            ParametersNode* params = (ParametersNode*)node;
+            for(int i = 0 ; i < params->count; i++){
+                generate_code(params->parameters[i]);
+                if(params->count != 1 && i != params->count - 1){
+                    fprintf(output_file,", ");
+                }
+            }
+            break;
+        }
+        case DECLARATION_NODE: {
+            DeclarationNode* declaration = (DeclarationNode*)node;
+            generate_code(declaration->type_specifier);
+            fprintf(output_file, " ");
+            generate_code(declaration->identifier);
+            if(declaration->initializer->type != EMPTY_STATEMENT_NODE){
+                fprintf(output_file," = ");
+                generate_code(declaration->initializer);
+            }
+            if (current_context == CONTEXT_DEFAULT) {
+                fprintf(output_file, ";");
+            }
+            break;
+        }
+        case COMPOUND_STATEMENT_NODE: {
+            CompoundStatementNode* compound_statement = (CompoundStatementNode*)node;
+            for(int i = 0 ; i < compound_statement->count; i++){
+                generate_code(compound_statement->statements[i]);
+                if(compound_statement->statements[i]->type != EMPTY_STATEMENT_NODE){
+                    fprintf(output_file, "\n");
+                }
+            }
+            break;
+        }
+        case POINTER_NODE: {
+            PointerNode* pointer = (PointerNode*)node;
+            fprintf(output_file, "*");
+            generate_code(pointer->declarator);
+            break;
+        }
+        case STRING_LITERAL_NODE: {
+            StringNode* string = (StringNode*)node;
+            fprintf(output_file,"%s",string->value);
+        }
+        case CONSTANT_NODE: {
+            ConstantNode* constant = (ConstantNode*)node;
+            if(constant->const_type == CONST_INT){
+                fprintf(output_file,"%d",constant->value.i_value);
+            }else if(constant->const_type == CONST_FLOAT){
+                fprintf(output_file,"%f",constant->value.f_value);
+            }
+            break;
+        }
+        case EMPTY_STATEMENT_NODE: {
+            break;
+        }
+        case IDENTIFIER_NODE: {
+            IdentifierNode* id_node = (IdentifierNode*)node;
+            fprintf(output_file, "%s", id_node->name);
+            break;
+        }
+        default: {
+            break;
+        }
     }
+}
+
+void generate_code_from_ast(Node* root, FILE* output) {
+    if (!root) return;
+    label_counter = 1;
+    output_file = output;
+    generate_code(root);
 }
